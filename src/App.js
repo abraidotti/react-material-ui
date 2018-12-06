748c7daacef96adf85066f1517b212253b64901e
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-06 09:06:40 -0500

    add working app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
-// import './App.css';
+import CourseList from './components/CourseList'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <CourseList />
       </div>
     )
   }
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -5,8 +5,8 @@ import * as contentful from 'contentful'
 
 import Course from '../components/Course'
 
-const SPACE_ID = '[INSERT CONTENTFUL SPACE ID]'
-const ACCESS_TOKEN = '[INSERT CONTENTFUL ACCESS TOKEN]'
+const SPACE_ID = 't90p20lpakuv'
+const ACCESS_TOKEN = '2986699a156e0adf1e87811d5811e03a989e48c458360c14810059668f9880ee'
 
 const client = contentful.createClient({
   space: SPACE_ID,
@@ -65,7 +65,7 @@ class CourseList extends Component {
               { this.state.courses.map(currentCourse => (
                 <Grid item xs={12} sm={6} lg={4} xl={3}>
                   <Course course={currentCourse} />
-                </Grid
+                </Grid>
               ))}
             </Grid>
           </div>
@@ -74,3 +74,5 @@ class CourseList extends Component {
     )
   }
 }
+
+export default CourseList
